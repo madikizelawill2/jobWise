bb27e5de0e26da76ab3b835f49fe536097c55685
Will <madikizelawill2@gmail.com>
2023-11-28 21:14:03 +0200

    [ADDED] constants for easy access and re-usability

--- jobwise/Components/Navbar.tsx
+++ jobwise/Components/Navbar.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="border-10 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
+        <Link href="/">
+            <h2 className="text-3xl font-bold">JobWise</h2>
+            <ul className="hidden h-full gap-12 lg:flex">
+              
+            </ul>
+        </Link>
+    </nav>
   )
 }
 
---
+++ jobwise/constants/index.ts
@@ -0,0 +1,7 @@
+
+export const NAV_LINKS = [
+    {href: '/', key: 'home', label: 'Home'},
+    {href: '/', key: 'findjob', label: 'Find Job'},
+    {href: '/', key: 'postjob', label: 'Post Job'},
+    {href: '/', key: 'categories', label: 'Categories'},
+];
