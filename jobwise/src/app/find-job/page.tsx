0155c2c63da554398b0a5122954f6dfaeafb63e6
Will <madikizelawill2@gmail.com>
2023-11-28 14:57:04 +0200

    [ADDED] helper function and packages for styling.

--- jobwise/src/app/find-job/page.tsx
+++ jobwise/src/app/find-job/page.tsx
@@ -1,7 +1,7 @@
 
 import Image from 'next/image'
 
-export default function Home() {
+export default function findJob() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       find job page
