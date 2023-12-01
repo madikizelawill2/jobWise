59e01dabc6c589bfb69a4c13a5f2e39b481d54ea
Will <madikizelawill2@gmail.com>
2023-12-01 15:40:00 +0200

    [FIXED] tailwindcss is fixed.

--- jobwise/Components/Navbar.tsx
+++ jobwise/Components/Navbar.tsx
@@ -3,10 +3,10 @@ import { NAV_LINKS } from "../constants"
 
 const Navbar = () => {
   return (
-    <nav className="border-10 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
+    <nav className="border-4 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
         <Link href="/">
             <h2 className="text-3xl font-bold">JobWise</h2>
-            <ul className="hidden h-full gap-12 lg:flex">
+            <ul className="font-bold hidden h-full gap-12 lg:flex">
               {NAV_LINKS.map((link) => (
                 <Link href={link.href} key={link.href}>
                     {link.label}
--- jobwise/src/app/page.tsx
+++ jobwise/src/app/page.tsx
@@ -1,8 +1,10 @@
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-17">
       main page
     </main>
   )
 }
+
+
